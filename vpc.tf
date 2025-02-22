216aa2fb2ac6cc5c0291f71d484be591549bca4d
eksuser <ragupathi121290@gmail.com>
2025-02-22 12:15:47 +0530

    error fixed

--- vpc.tf
+++ vpc.tf
@@ -49,8 +49,8 @@ resource "aws_route_table_association" "public_assoc_1" {
 }
 
 resource "aws_route_table_association" "public_assoc_2" {
-  subnet_id = aws_subnet.public_subnet_2
-  route_table_id = aws_route_table.public_rt
+  subnet_id = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_eip" "nat_eip" {
