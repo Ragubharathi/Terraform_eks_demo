15afea0b03bf74f83aadbe296ce0a26599c43eaf
eksuser <ragupathi121290@gmail.com>
2025-02-24 19:14:16 +0530

    added

--- practice/main.tf
+++ practice/main.tf
@@ -28,9 +28,13 @@ resource "aws_security_group" "securitygroup" {
 resource "aws_instance" "ec2-aws_instance" {
   instance_type = "t2.micro"
   ami = "ami-0d682f26195e9ec0f"
-  subnet_id = aws_subnet.public_sub_1
-  security_groups = aws_security_group.securitygroup.id
+  subnet_id = aws_subnet.public_sub_1.id
+  security_groups = [aws_security_group.securitygroup.name]
   tags = {
     Name = "ec2_instance_terraform"
   }
+}
+
+resource "aws_s3_bucket" "se_bucket" {
+  bucket = "ragu-s3-demo-xyz"
 }
