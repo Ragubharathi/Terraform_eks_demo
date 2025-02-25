2d0f75a5615d8f4ad37a20bd3c76e91af3943812
eksuser <ragupathi121290@gmail.com>
2025-02-25 12:23:31 +0530

    changed

--- practice/backend.tf
+++ practice/backend.tf
@@ -1,7 +1,7 @@
-*/terraform {
+terraform {
   backend "s3" {
-    bucket = "ragu-s3-demo-xyz"
+    bucket = "ragu-demo-terraform-test-xyz"
     key = "ragu/terraform.tfstate"
     region = "ap-south-1"
   }
-}*/
+}
